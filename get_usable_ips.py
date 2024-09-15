b4307b7c8ef21713d266616e168f1814b903530f
fla5h0v3rr1de <149769687+fla5h0v3rr1de@users.noreply.github.com>
2024-09-16 02:28:41 +0800

    Add files via upload
    initial commit

---
+++ get_usable_ips.py
@@ -0,0 +1,45 @@
+import ipaddress
+import re
+from datetime import date
+
+# User input subnet
+user_input = str(input("Enter a valid IP address and subnet in the format x.x.x.x/y: "))
+
+# Checks the format from the user input
+def get_valid_subnet():
+    while True:
+             
+        # Validate user input format
+        pattern = r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/\d{1,2}$'
+
+        if re.fullmatch(pattern, user_input):
+            try:
+                # Create an IPv4 network object
+                network_block = ipaddress.IPv4Network(user_input, strict=False)
+                # Return list of usable IP addresses
+                host_ips = [str(ip) for ip in network_block.hosts()]
+                return host_ips
+            except ValueError:
+                print("Invalid subnet range. Please Enter correct subnet range format.")
+                return False
+        else:
+            print("Invalid format. Please enter valid IP address or input only one instance of a subnet format in x.x.x.x/y.")
+            return False
+
+# Generate usable host IP address
+usable_ips = get_valid_subnet()
+
+# Generate output file path
+today = date.today()
+replace_string = user_input.replace('/', '_')
+output_file_path = replace_string + "_" + str(today)
+
+# Save the generated lists of usable host IP addresses in a file
+if usable_ips == False :
+    print("Unable to print result....")    
+else:
+    with open(output_file_path, "w") as f:
+           for ip in usable_ips:
+               f.write(ip + "\n")
+
+    print(f"Completed generating usable host IPs. Check {output_file_path} for result")
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Get Usable IPs
+
+This tool helps to get the usable IP Addresses (Host Addresses), which will be used to assign to host(s) or to discover which are the possible active host(s) on a given subnet 
+
+
